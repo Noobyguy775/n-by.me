afd1d92421a0c07845524d28e94631452f56873e
Nooby <109582490+Noobyguy775@users.noreply.github.com>
2025-09-18 00:06:45 -0700

    beautiful avatar

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+
 export default function projects() {
-  return <>{"Nothing's here yet! You'll check back later, right?"}</>;
+  return <>
+    <Image
+      src="/files/beautiful_avatar.png"
+      alt="Beautiful Avatar"
+      width={128}
+      height={128}
+    />
+    <p>
+      {"Nothing's here yet! You'll check back later, right?"}
+    </p>
+  </>;
 }
